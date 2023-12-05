7b67a16f8023380fb5dcb51e2a39c7f5e0ec4691
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2023-12-05 08:57:00 -0300

    add Dashboard and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
 
   return (
     <>
-    <Header />
+      <Header />
+      <Dashboard />
     </>
   )
 }
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import "./style.css";
-import logo from "../../../public/images/logo.png";
+import logo from "../../assets/images/logo.png";
 
 export function Logotipo() {
   return (
