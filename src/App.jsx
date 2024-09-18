e1d9fadcf75fea009b2c38a0a04f0da943d305c6
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2024-09-18 18:25:20 -0300

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useTab } from './context/TabContext';
 import { RoutesTabs } from './tools/RoutesTabs';
 import { FilterDate } from './components/FilterDate';
 import { AuthPopup } from './components/AuthPopup';
+import { BackToTop } from './components/BackToTop';
+import { Toolbar } from '@mui/material';
 
 function App() {
   const { activeTab } = useTab();
@@ -12,12 +14,14 @@ function App() {
   return (
     <>
       <AuthPopup />
+      <Toolbar style={{minHeight: '0px'}} id="back-to-top-anchor" />
       <Header />
       <Footer />
       <div className={activeTab === 3 ? 'hidden' : ''}>
         <FilterDate />
       </div>
       <RoutesTabs activeTab={activeTab} />
+      <BackToTop />
     </>
   );
 }
---
+++ src/components/BackToTop/index.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import PropTypes from 'prop-types';
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+export function BackToTop(props) {
+  return (
+    <>
+      <ScrollTop {...props}>
+        <Fab color='primary' size="small" aria-label="Subir para o topo da página">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </>
+  );
+}
