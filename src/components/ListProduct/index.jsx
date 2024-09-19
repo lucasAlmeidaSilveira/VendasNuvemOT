333c2508b822428dab5270efd967abf8b0f4a6bd
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2024-09-19 13:57:18 -0300

    remove requests canonical_url

--- src/components/ListProduct/index.jsx
+++ src/components/ListProduct/index.jsx
@@ -4,32 +4,13 @@ import { getProduct } from "../../api";
 import { useOrders } from "../../context/OrdersContext";
 
 export function ListProduct({ idProduct, position, name, skuNumber, variations, urlImage, sales }) {
-  const { store } = useOrders();
-  const [urlProduct, setUrlProduct] = useState(''); // Estado para armazenar a URL do produto
-
-  useEffect(() => {
-    if (urlImage) {
-      // Função assíncrona para buscar a URL do produto
-      const fetchProductUrl = async () => {
-        try {
-          const { canonical_url } = await getProduct(store, idProduct);
-          setUrlProduct(canonical_url); // Atualiza o estado com a URL obtida
-        } catch (error) {
-          // console.error("Failed to fetch product URL:", error);
-          setUrlProduct(""); // Em caso de erro, define a URL como string vazia
-        }
-      };
-
-      fetchProductUrl(); // Chama a função assíncrona
-    }
-  }, [store, idProduct, urlImage]);
 
   return (
     <Container>
       <div className="frame">
         {urlImage && <img className="image-product" src={urlImage} alt={name} />}
         {urlImage ? (
-          <a href={urlProduct} target="_blank" className="info-product">
+          <a href={urlImage} target="_blank" className="info-product">
             <p className="text-position">#{position}</p>
             <p className="name-product">{skuNumber ? skuNumber + ' | ' : ''} {name}</p>
             {variations && <p className="text-variant">{variations}</p>}
