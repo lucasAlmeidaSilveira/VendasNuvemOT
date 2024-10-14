903f3c15d98ced3edff934beafb5de5075a571f5
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2024-10-14 11:30:31 -0300

    add checkbox display coupons sellers

--- src/components/Coupons/index.jsx
+++ src/components/Coupons/index.jsx
@@ -14,6 +14,7 @@ import { TablePaginationActions } from '../Pagination';
 import { formatDateToISO } from '../../tools/tools';
 import { Loading } from '../Loading';
 import { filterOrders } from '../../tools/filterOrders';
+import { Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -58,13 +59,17 @@ export function Coupons() {
   const [filteredCoupons, setFilteredCoupons] = useState([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [isSellerFilterActive, setIsSellerFilterActive] = useState(false);
+
+  // Cupons de vendedores
+  const couponsSellers = ['CIBELEAP', 'CLAUDIOAP', 'DIEGOAP', 'IAGOAP', 'YARAAP'];
 
   useEffect(() => {
     const startDateISO = formatDateToISO(date[0]);
     const endDateISO = formatDateToISO(date[1]);
-  
+
     const couponUsageMap = {};
-  
+
     ordersToday.forEach(order => {
       const orderDate = new Date(order.created_at);
       if (
@@ -72,6 +77,10 @@ export function Coupons() {
         orderDate <= new Date(endDateISO)
       ) {
         order.coupon.forEach(coupon => {
+          if (isSellerFilterActive && !couponsSellers.includes(coupon.code)) {
+            return; // Pula cupons que não são dos vendedores se o filtro estiver ativo
+          }
+
           if (!couponUsageMap[coupon.code]) {
             couponUsageMap[coupon.code] = {
               ...coupon,
@@ -84,13 +93,13 @@ export function Coupons() {
         });
       }
     });
-  
+
     const sortedCoupons = Object.values(couponUsageMap).sort(
       (a, b) => b.used - a.used,
     );
-  
+
     setFilteredCoupons(sortedCoupons);
-  }, [date, store, allOrders]);  
+  }, [date, store, allOrders, isSellerFilterActive]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -106,89 +115,113 @@ export function Coupons() {
     setPage(0);
   };
 
+  const handleSellerFilterChange = event => {
+    setIsSellerFilterActive(event.target.checked);
+  };
+
   return (
-    <ContainerCoupon component={Paper}>
-      <Table aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Código</StyledTableCell>
-            <StyledTableCell>Desconto</StyledTableCell>
-            <StyledTableCell>Valor</StyledTableCell>
-            <StyledTableCell>Usado</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {isLoading ? (
+    <>
+      <FormGroup>
+        <FormControlLabel 
+          control={
+            <Checkbox
+              checked={isSellerFilterActive}
+              onChange={handleSellerFilterChange}
+              inputProps={{ 'aria-label': 'controlled' }}
+              size='large'
+            />
+          } 
+          label={
+            <Typography variant="subtitle1" style={{ fontFamily: 'Poppins', color: 'var(--geralblack-100)', fontSize: 12 }}>
+              Mostrar somente cupons de vendedores
+            </Typography>
+          }
+          
+        />
+      </FormGroup>
+      <ContainerCoupon component={Paper}>
+        <Table aria-label='simple table'>
+          <TableHead>
             <TableRow>
-              <StyledTableCell style={{ textAlign: 'center' }} colSpan={4}>
-                <Loading />
-              </StyledTableCell>
+              <StyledTableCell>Código</StyledTableCell>
+              <StyledTableCell>Desconto</StyledTableCell>
+              <StyledTableCell>Valor</StyledTableCell>
+              <StyledTableCell>Usado</StyledTableCell>
             </TableRow>
-          ) : filteredCoupons.length === 0 ? (
-            <TableRow>
-              <StyledTableCell style={{ textAlign: 'center' }} colSpan={7}>
-                Nenhum cupom usado
-              </StyledTableCell>
-            </TableRow>
-          ) : (
-            filteredCoupons
+          </TableHead>
+          <TableBody>
+            {isLoading ? (
+              <TableRow>
+                <StyledTableCell style={{ textAlign: 'center' }} colSpan={4}>
+                  <Loading />
+                </StyledTableCell>
+              </TableRow>
+            ) : filteredCoupons.length === 0 ? (
+              <TableRow>
+                <StyledTableCell style={{ textAlign: 'center' }} colSpan={7}>
+                  Nenhum cupom usado
+                </StyledTableCell>
+              </TableRow>
+            ) : (
+              filteredCoupons
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map(coupon => (
-                <StyledTableRow
+                  <StyledTableRow
                   key={coupon.id}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <StyledTableCell component='th' scope='row'>
-                    {coupon.code}
-                  </StyledTableCell>
-                  {coupon.type === 'percentage' ? (
-                    <StyledTableCell>{`${parseInt(coupon.value)}%`}</StyledTableCell>
-                  ) : (
-                    <StyledTableCell>{`R$ ${parseFloat(coupon.value).toFixed(2).replace('.', ',')}`}</StyledTableCell>
-                  )}
-                  <StyledTableCell>{`R$ ${coupon.totalRevenue.toFixed(2).replace('.', ',')}`}</StyledTableCell>
-                  <StyledTableCell>{coupon.used}</StyledTableCell>
-                </StyledTableRow>
-              ))
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 20, 50]}
-              colSpan={7}
-              count={filteredCoupons.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-              labelRowsPerPage='Linhas por página:'
-              labelDisplayedRows={({ from, to, count }) =>
+                  >
+                    <StyledTableCell component='th' scope='row'>
+                      {coupon.code}
+                    </StyledTableCell>
+                    {coupon.type === 'percentage' ? (
+                      <StyledTableCell>{`${parseInt(coupon.value)}%`}</StyledTableCell>
+                      ) : (
+                        <StyledTableCell>{`R$ ${parseFloat(coupon.value).toFixed(2).replace('.', ',')}`}</StyledTableCell>
+                        )}
+                    <StyledTableCell>{`R$ ${coupon.totalRevenue.toFixed(2).replace('.', ',')}`}</StyledTableCell>
+                    <StyledTableCell>{coupon.used}</StyledTableCell>
+                  </StyledTableRow>
+                ))
+                )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 20, 50]}
+                colSpan={7}
+                count={filteredCoupons.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+                labelRowsPerPage='Linhas por página:'
+                labelDisplayedRows={({ from, to, count }) =>
                 `${from}–${to} de ${count}`
               }
               sx={{
-                '& .MuiTablePagination-toolbar': {
-                  fontSize: '1.1rem',
-                  fontFamily: 'Poppins, sans-serif',
-                },
-                '& .MuiTablePagination-selectLabel': {
-                  fontSize: '1.1rem',
-                  fontFamily: 'Poppins, sans-serif',
-                },
-                '& .MuiTablePagination-input': {
-                  fontSize: '1.1rem',
-                  fontFamily: 'Poppins, sans-serif',
-                },
-                '& .MuiTablePagination-displayedRows': {
-                  fontSize: '1.1rem',
-                  fontFamily: 'Poppins, sans-serif',
-                },
-              }}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </ContainerCoupon>
+                  '& .MuiTablePagination-toolbar': {
+                    fontSize: '1.1rem',
+                    fontFamily: 'Poppins, sans-serif',
+                  },
+                  '& .MuiTablePagination-selectLabel': {
+                    fontSize: '1.1rem',
+                    fontFamily: 'Poppins, sans-serif',
+                  },
+                  '& .MuiTablePagination-input': {
+                    fontSize: '1.1rem',
+                    fontFamily: 'Poppins, sans-serif',
+                  },
+                  '& .MuiTablePagination-displayedRows': {
+                    fontSize: '1.1rem',
+                    fontFamily: 'Poppins, sans-serif',
+                  },
+                }}
+                />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </ContainerCoupon>
+    </>
   );
 }
