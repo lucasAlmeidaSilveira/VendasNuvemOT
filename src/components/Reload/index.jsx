cddecea4a4156f77743afc2d623a03a741641ca4
phr31 <phr31@hotmail.com>
2025-03-19 09:54:24 -0300

    Add reloadRefunds to ButtonReload

--- src/components/Reload/index.jsx
+++ src/components/Reload/index.jsx
@@ -1,26 +1,29 @@
-import React from "react";
-import "./style.css";
-import { IoReloadCircleSharp } from "react-icons/io5";
-import { useOrders } from "../../context/OrdersContext";
-import { useAnalytics } from "../../context/AnalyticsContext";
-import { useAuth } from "../../context/AuthContext";
+import React from 'react';
+import './style.css';
+import { IoReloadCircleSharp } from 'react-icons/io5';
+import { useOrders } from '../../context/OrdersContext';
+import { useAnalytics } from '../../context/AnalyticsContext';
+import { useAuth } from '../../context/AuthContext';
+import { useRefunds } from '../../context/RefundsContext';
 
 export function ButtonReload() {
   const { isLoading, fetchData } = useOrders();
   const { fetchDataGoogle, fetchDataADSMeta } = useAnalytics();
-  const { user } = useAuth()
+  const { reloadRefunds } = useRefunds();
+  const { user } = useAuth();
 
   const handleReload = () => {
-    if(user) {
-      fetchData()
+    if (user) {
+      fetchData();
       fetchDataGoogle();
       fetchDataADSMeta();
+      reloadRefunds();
     }
   };
 
   return (
     <button
-      className={`boxReload ${(isLoading) && "loading"}`}
+      className={`boxReload ${isLoading && 'loading'}`}
       onClick={handleReload}
       aria-label="Recarregar dados"
     >
